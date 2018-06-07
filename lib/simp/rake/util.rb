822c4eef02c32180db008859eed9aa1af2ec8e1f
Nick Miller <nick.miller@onyxpoint.com>
2018-06-07 15:56:31 -0400

    add yaml and json validators

--- lib/simp/rake/util.rb
+++ lib/simp/rake/util.rb
@@ -23,12 +23,54 @@ module Simp::Rake
       puts params.flatten.compact.join("\n")
     end
 
+    def validate_json
+      require 'json'
+      Dir.glob("**/*.json").map do |json_file|
+        print "Attempting to load #{json_file} ... "
+        begin
+          JSON.load(File.new(json_file))
+          print 'ok'
+        rescue JSON::ParserError => e
+          print "failed: #{e}"
+          exit 1
+        end
+        puts
+      end
+    end
+
+    def validate_yaml
+      require 'yaml'
+      Dir.glob("**/*.y{,a}ml").map do |yaml_file|
+        print "Attempting to load #{yaml_file} ... "
+
+        begin
+          YAML.load(File.new(yaml_file))
+          print 'ok'
+        rescue Psych::SyntaxError => e
+          print "failed: #{e}"
+          exit 1
+        end
+        puts
+      end
+    end
+
     def define
       namespace :util do
         desc 'Run puppet strings, and print all class params in their namespace'
         task :get_params do
           get_params
         end
+
+        desc 'Validate all json files in working directory'
+        task :validate_json do
+          validate_json
+        end
+
+        desc 'Validate all yaml files in working directory'
+        task :validate_yaml do
+          validate_yaml
+        end
+
       end
     end
   end
